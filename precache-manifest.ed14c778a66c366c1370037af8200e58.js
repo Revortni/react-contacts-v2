f21be5194ac760bd43dd70f49c3e0fcc57e697b3
revortni <rojit.blaze13@gmail.com>
2020-01-09 21:09:24 +0545

    Updates

---
+++ precache-manifest.ed14c778a66c366c1370037af8200e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f443c1088a36e68d429a8635d0a2065f",
+    "url": "/react-contacts-v2/index.html"
+  },
+  {
+    "revision": "0c4052b6db0bda80ac27",
+    "url": "/react-contacts-v2/static/css/main.6658cfe1.chunk.css"
+  },
+  {
+    "revision": "dd7aed189a44cb8f8abb",
+    "url": "/react-contacts-v2/static/js/2.56e97a72.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-contacts-v2/static/js/2.56e97a72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c4052b6db0bda80ac27",
+    "url": "/react-contacts-v2/static/js/main.cc43bf35.chunk.js"
+  },
+  {
+    "revision": "223ec530a979a79dd252",
+    "url": "/react-contacts-v2/static/js/runtime-main.0c4358b2.js"
+  }
+]);
